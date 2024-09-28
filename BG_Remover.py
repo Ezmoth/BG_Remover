3310f418616f49ecfdc1375304c8122667ce4cb6
Ezmoth <tomaszdziduch@gmail.com>
2024-09-28 13:40:12 +0100

    Removing unnecesary files, renaming the files and adjusting the code.

--- BG_Remover.py
+++ BG_Remover.py
@@ -1,13 +1,14 @@
 import os
 from PIL import Image
 import rembg
-import numpy as np
+import Parts
 
 # Core engine of the program
 def remover():
     # Initialize all variables
     to_process_folder = "To Process\\"
     done_folder = "Done\\"
+    parts_folder = "Res\\"
     file_name_list = [f for f in os.listdir("To Process") if os.path.isfile(os.path.join("To Process", f))]
     n = 0
     # Show how many files are there to process
@@ -17,29 +18,19 @@ def remover():
         n += 1 # Counter for displaying progress
         to_do_pic = Image.open(to_process_folder + pic)
         processed_pic = rembg.remove(to_do_pic)
-
-        # Numpy magic! Makes an array from picture
-        pic_np = np.array(processed_pic)
-        rows = np.any(pic_np, axis=1)
-        cols = np.any(pic_np, axis=0)
-        rows_min, rows_max = np.where(rows)[0][[0, -1]]
-        cols_min, cols_max = np.where(cols)[0][[0, -1]]
-
-        # Finding the dimensions of an object and cropping to 10px on each side
-        rows_min = max(0, rows_min - 10)
-        rows_max = min(pic_np.shape[0], rows_max + 10)
-        cols_min = max(0, cols_min -10)
-        cols_max = min(pic_np.shape[1], cols_max + 10)
-
-        # Cropping the picture and converting it from array back to image
-        cropped_pic_np = pic_np[rows_min:rows_max, cols_min:cols_max]
-        cropped_pic = Image.fromarray(cropped_pic_np)
-
-        # Creating white background, pasting the image on top and saving to "Done" folder
-        white_bg = Image.new("RGB", cropped_pic.size, (255, 255, 255))
-        white_bg.paste(cropped_pic, mask=cropped_pic.split()[3])
-        white_bg.save(done_folder + pic)
-
+        if pic[-6:-4].upper() == "P2":
+            Parts.parts_2(done_folder, parts_folder, processed_pic, pic)
+        elif pic[-6:-4].upper() == "P3":
+            Parts.parts_3(done_folder, parts_folder, processed_pic, pic)
+        elif pic[-6:-4].upper() == "P4":
+            Parts.parts_4(done_folder, parts_folder, processed_pic, pic)
+        elif pic[-6:-4].upper() == "P5":
+            Parts.parts_5(done_folder, parts_folder, processed_pic, pic)
+        elif pic[-6:-4].upper() == "P6":
+            Parts.parts_6(done_folder, parts_folder, processed_pic, pic)
+        else:
+            Parts.parts_1(done_folder, processed_pic, pic)
+        
         # Show progress
         print(f"Image {n} of {len(file_name_list)} processed.")
 
@@ -51,8 +42,6 @@ def remover():
 # it closes the app after pressing "ENTER" key
 print("Make sure all files are in \"To Process\" folder.")
 start = input("Start the program? (Y/N): ").upper()
-while start[0] == "Y":
+while start == "Y":
     remover()
     start = input("Press \"ENTER\" to exit or \"Y\" to start again: ").upper()
-
-
--- Parts.py
+++ Parts.py
@@ -1,47 +1,203 @@
-import os
 from PIL import Image
-import rembg
-import Test_Parts
+import numpy as np
 
-# Core engine of the program
-def remover():
-    # Initialize all variables
-    to_process_folder = "To Process\\"
-    done_folder = "Done\\"
-    parts_folder = "Res\\"
-    file_name_list = [f for f in os.listdir("To Process") if os.path.isfile(os.path.join("To Process", f))]
-    n = 0
-    # Show how many files are there to process
-    print(f"Files to process: {len(file_name_list)}")
 
-    for pic in file_name_list:
-        n += 1 # Counter for displaying progress
-        to_do_pic = Image.open(to_process_folder + pic)
-        processed_pic = rembg.remove(to_do_pic)
-        if pic[-6:-4].upper() == "P2":
-            Test_Parts.parts_2(done_folder, parts_folder, processed_pic, pic)
-        elif pic[-6:-4].upper() == "P3":
-            Test_Parts.parts_3(done_folder, parts_folder, processed_pic, pic)
-        elif pic[-6:-4].upper() == "P4":
-            Test_Parts.parts_4(done_folder, parts_folder, processed_pic, pic)
-        elif pic[-6:-4].upper() == "P5":
-            Test_Parts.parts_5(done_folder, parts_folder, processed_pic, pic)
-        elif pic[-6:-4].upper() == "P6":
-            Test_Parts.parts_6(done_folder, parts_folder, processed_pic, pic)
-        else:
-            Test_Parts.parts_1(done_folder, processed_pic, pic)
-        
-        # Show progress
-        print(f"Image {n} of {len(file_name_list)} processed.")
+def parts_1(done_folder, processed_pic, pic):
+    # Numpy magic! Makes an array from picture
+    pic_np = np.array(processed_pic)
+    rows = np.any(pic_np, axis=1)
+    cols = np.any(pic_np, axis=0)
+    rows_min, rows_max = np.where(rows)[0][[0, -1]]
+    cols_min, cols_max = np.where(cols)[0][[0, -1]]
 
-    # Message when finished
-    print("\nDone!")
-    print("-" * 30 + "\n")
+    # Finding the dimensions of an object and cropping to 10px on each side of an object
+    rows_min = max(0, rows_min - 10)
+    rows_max = min(pic_np.shape[0], rows_max + 10)
+    cols_min = max(0, cols_min -10)
+    cols_max = min(pic_np.shape[1], cols_max + 10)
 
-# Menu - for now if input doesn't start at "y" 
-# it closes the app after pressing "ENTER" key
-print("Make sure all files are in \"To Process\" folder.")
-start = input("Start the program? (Y/N): ").upper()
-while start == "Y":
-    remover()
-    start = input("Press \"ENTER\" to exit or \"Y\" to start again: ").upper()
+    # Cropping the picture and converting it from array back to image
+    cropped_pic_np = pic_np[rows_min:rows_max, cols_min:cols_max]
+    cropped_pic = Image.fromarray(cropped_pic_np)
+
+    # Creating white background, pasting the image on top and saving to "Done" folder
+    white_bg = Image.new("RGB", cropped_pic.size, (255, 255, 255))
+    white_bg.paste(cropped_pic, mask=cropped_pic.split()[3])
+    white_bg.save(done_folder + pic)
+
+    return white_bg
+
+def parts_2(done_folder, parts_folder, processed_pic, pic):
+    numbers = parts_folder + "2.png"
+    process_part1 = Image.open(numbers)
+    # Numpy magic! Makes an array from picture
+    pic_np = np.array(processed_pic)
+    rows = np.any(pic_np, axis=1)
+    cols = np.any(pic_np, axis=0)
+    # Finding the dimensions of an object
+    rows_min, rows_max = np.where(rows)[0][[0, -1]]
+    cols_min, cols_max = np.where(cols)[0][[0, -1]]
+
+    # Finding the dimensions of an object and cropping to 10px on each side of an object
+    rows_min = max(0, rows_min - 10)
+    rows_max = min(pic_np.shape[0], rows_max + 10)
+    cols_min = max(0, cols_min -10)
+    cols_max = min(pic_np.shape[1], cols_max + 10)
+
+    # Cropping the picture and converting it from array back to image
+    cropped_pic_np = pic_np[rows_min:rows_max, cols_min:cols_max]
+    cropped_pic = Image.fromarray(cropped_pic_np)
+
+    # Creating white background, pasting the image on top and saving to "Done" folder
+    white_bg = Image.new("RGB", cropped_pic.size, (255, 255, 255))
+    white_bg.paste(cropped_pic, mask=cropped_pic.split()[3])
+
+    width_img = max(white_bg.size[0], process_part1.size[0])
+    height_img = white_bg.size[1] + process_part1.size[1]
+    parts_image = Image.new("RGB", (width_img, height_img), (255, 255, 255))
+
+    parts_image.paste(white_bg)
+    parts_image.paste(process_part1, ((white_bg.size[0]//2 - process_part1.size[0]//2), white_bg.size[1]))
+    parts_image.save(done_folder + pic)
+
+    return parts_image
+
+def parts_3(done_folder, parts_folder, processed_pic, pic):
+    numbers = parts_folder + "3.png"
+    process_part1 = Image.open(numbers)
+    # Numpy magic! Makes an array from picture
+    pic_np = np.array(processed_pic)
+    rows = np.any(pic_np, axis=1)
+    cols = np.any(pic_np, axis=0)
+    # Finding the dimensions of an object
+    rows_min, rows_max = np.where(rows)[0][[0, -1]]
+    cols_min, cols_max = np.where(cols)[0][[0, -1]]
+
+    # Finding the dimensions of an object and cropping to 10px on each side of an object
+    rows_min = max(0, rows_min - 10)
+    rows_max = min(pic_np.shape[0], rows_max + 10)
+    cols_min = max(0, cols_min -10)
+    cols_max = min(pic_np.shape[1], cols_max + 10)
+
+    # Cropping the picture and converting it from array back to image
+    cropped_pic_np = pic_np[rows_min:rows_max, cols_min:cols_max]
+    cropped_pic = Image.fromarray(cropped_pic_np)
+
+    # Creating white background, pasting the image on top and saving to "Done" folder
+    white_bg = Image.new("RGB", cropped_pic.size, (255, 255, 255))
+    white_bg.paste(cropped_pic, mask=cropped_pic.split()[3])
+
+    width_img = max(white_bg.size[0], process_part1.size[0])
+    height_img = white_bg.size[1] + process_part1.size[1]
+    parts_image = Image.new("RGB", (width_img, height_img), (255, 255, 255))
+
+    parts_image.paste(white_bg)
+    parts_image.paste(process_part1, ((white_bg.size[0]//2 - process_part1.size[0]//2), white_bg.size[1]))
+    parts_image.save(done_folder + pic)
+
+    return parts_image
+
+def parts_4(done_folder, parts_folder, processed_pic, pic):
+    numbers = parts_folder + "4.png"
+    process_part1 = Image.open(numbers)
+    # Numpy magic! Makes an array from picture
+    pic_np = np.array(processed_pic)
+    rows = np.any(pic_np, axis=1)
+    cols = np.any(pic_np, axis=0)
+    # Finding the dimensions of an object
+    rows_min, rows_max = np.where(rows)[0][[0, -1]]
+    cols_min, cols_max = np.where(cols)[0][[0, -1]]
+
+    # Finding the dimensions of an object and cropping to 10px on each side of an object
+    rows_min = max(0, rows_min - 10)
+    rows_max = min(pic_np.shape[0], rows_max + 10)
+    cols_min = max(0, cols_min -10)
+    cols_max = min(pic_np.shape[1], cols_max + 10)
+
+    # Cropping the picture and converting it from array back to image
+    cropped_pic_np = pic_np[rows_min:rows_max, cols_min:cols_max]
+    cropped_pic = Image.fromarray(cropped_pic_np)
+
+    # Creating white background, pasting the image on top and saving to "Done" folder
+    white_bg = Image.new("RGB", cropped_pic.size, (255, 255, 255))
+    white_bg.paste(cropped_pic, mask=cropped_pic.split()[3])
+
+    width_img = max(white_bg.size[0], process_part1.size[0])
+    height_img = white_bg.size[1] + process_part1.size[1]
+    parts_image = Image.new("RGB", (width_img, height_img), (255, 255, 255))
+
+    parts_image.paste(white_bg)
+    parts_image.paste(process_part1, ((white_bg.size[0]//2 - process_part1.size[0]//2), white_bg.size[1]))
+    parts_image.save(done_folder + pic)
+
+    return parts_image
+
+def parts_5(done_folder, parts_folder, processed_pic, pic):
+    numbers = parts_folder + "5.png"
+    process_part1 = Image.open(numbers)
+    # Numpy magic! Makes an array from picture
+    pic_np = np.array(processed_pic)
+    rows = np.any(pic_np, axis=1)
+    cols = np.any(pic_np, axis=0)
+    # Finding the dimensions of an object
+    rows_min, rows_max = np.where(rows)[0][[0, -1]]
+    cols_min, cols_max = np.where(cols)[0][[0, -1]]
+
+    # Finding the dimensions of an object and cropping to 10px on each side of an object
+    rows_min = max(0, rows_min - 10)
+    rows_max = min(pic_np.shape[0], rows_max + 10)
+    cols_min = max(0, cols_min -10)
+    cols_max = min(pic_np.shape[1], cols_max + 10)
+
+    # Cropping the picture and converting it from array back to image
+    cropped_pic_np = pic_np[rows_min:rows_max, cols_min:cols_max]
+    cropped_pic = Image.fromarray(cropped_pic_np)
+
+    # Creating white background, pasting the image on top and saving to "Done" folder
+    white_bg = Image.new("RGB", cropped_pic.size, (255, 255, 255))
+    white_bg.paste(cropped_pic, mask=cropped_pic.split()[3])
+
+    width_img = max(white_bg.size[0], process_part1.size[0])
+    height_img = white_bg.size[1] + process_part1.size[1]
+    parts_image = Image.new("RGB", (width_img, height_img), (255, 255, 255))
+
+    parts_image.paste(white_bg)
+    parts_image.paste(process_part1, ((white_bg.size[0]//2 - process_part1.size[0]//2), white_bg.size[1]))
+    parts_image.save(done_folder + pic)
+
+    return parts_image
+
+def parts_6(done_folder, parts_folder, processed_pic, pic):
+    numbers = parts_folder + "6.png"
+    process_part1 = Image.open(numbers)
+    # Numpy magic! Makes an array from picture
+    pic_np = np.array(processed_pic)
+    rows = np.any(pic_np, axis=1)
+    cols = np.any(pic_np, axis=0)
+    # Finding the dimensions of an object
+    rows_min, rows_max = np.where(rows)[0][[0, -1]]
+    cols_min, cols_max = np.where(cols)[0][[0, -1]]
+
+    # Finding the dimensions of an object and cropping to 10px on each side of an object
+    rows_min = max(0, rows_min - 10)
+    rows_max = min(pic_np.shape[0], rows_max + 10)
+    cols_min = max(0, cols_min -10)
+    cols_max = min(pic_np.shape[1], cols_max + 10)
+
+    # Cropping the picture and converting it from array back to image
+    cropped_pic_np = pic_np[rows_min:rows_max, cols_min:cols_max]
+    cropped_pic = Image.fromarray(cropped_pic_np)
+
+    # Creating white background, pasting the image on top and saving to "Done" folder
+    white_bg = Image.new("RGB", cropped_pic.size, (255, 255, 255))
+    white_bg.paste(cropped_pic, mask=cropped_pic.split()[3])
+
+    width_img = max(white_bg.size[0], process_part1.size[0])
+    height_img = white_bg.size[1] + process_part1.size[1]
+    parts_image = Image.new("RGB", (width_img, height_img), (255, 255, 255))
+
+    parts_image.paste(white_bg)
+    parts_image.paste(process_part1, ((white_bg.size[0]//2 - process_part1.size[0]//2), white_bg.size[1]))
+    parts_image.save(done_folder + pic)
+
+    return parts_image
